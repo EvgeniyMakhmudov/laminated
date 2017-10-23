dbd5996c89ad5fdd612851e15c18847555d37268
Evgeniy Makhmudov <john_16@list.ru>
2017-10-23 22:27:05 +0300

    Fix tests

--- laminated/tests/test_dict.py
+++ laminated/tests/test_dict.py
@@ -9,6 +9,6 @@ def test_get_dict_at_layer(l_minimum, layer_name, target):
     assert l_minimum.get_dict_at_layer(layer_name) == target
 
 
-@pytest.mark.xfail(raises=ValueError)
 def test_get_dict_at_layer_fail(l_minimum):
-    l_minimum.get_dict_at_layer('not_exists')
+    with pytest.raises(ValueError):
+        l_minimum.get_dict_at_layer('not_exists')
--- laminated/tests/test_main.py
+++ laminated/tests/test_main.py
@@ -6,9 +6,9 @@ def test__getitem__(l_minimum):
     assert l_minimum['a'] == 'A'
 
 
-@pytest.mark.xfail(raises=KeyError)
 def test__getitem__fail(l_minimum):
-    l_minimum['z']
+    with pytest.raises(KeyError):
+        l_minimum['z']
 
 
 def test_get_value_at_layer(l_minimum):
@@ -18,6 +18,6 @@ def test_get_value_at_layer(l_minimum):
     assert l_minimum.get_value_at_layer('fix', 'a') == l_minimum['a']
 
 
-@pytest.mark.xfail(raises=KeyError)
 def test_get_value_at_layer_fail(l_minimum):
-    l_minimum.get_value_at_layer('base', 'c')
+    with pytest.raises(KeyError):
+        l_minimum.get_value_at_layer('base', 'c')
