7f93e938057f6a7ab0eb91b29bc8ea3c5940c5f9
Evgeniy Makhmudov <john_16@list.ru>
2018-01-03 00:33:08 +0300

    Add one more test to increase coverage

--- laminated/tests/test_main.py
+++ laminated/tests/test_main.py
@@ -11,6 +11,14 @@ def test__getitem__fail(l_minimum):
         l_minimum['z']
 
 
+def test_dublicate_fail(l_minimum):
+    with pytest.raises(ValueError):
+        l_minimum.add_layer(
+            data={},
+            name='base'
+        )
+
+
 def test_get_value_at_layer(l_minimum):
     assert l_minimum.get_value_at_layer('base', 'b') == ''
     assert l_minimum.get_value_at_layer('fix', 'b') == 'B'
